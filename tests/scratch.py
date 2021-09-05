4e812d35e086ccb419b16813c28c492e336c468a
Dillon <loh.dillon@gmail.com>
2021-09-05 18:45:37 +0900

    added user input for coordinates of point1 and point 2 of each floor

--- tests/scratch.py
+++ tests/scratch.py
@@ -22,7 +22,12 @@ ax = fig.add_subplot(111, projection='3d')
 
 x, y = np.mgrid[0:map1.shape[0], 0:map1.shape[1]]
 z = np.atleast_2d(0)
-p1_1, p1_2 = (2841, 2284), (2783, 2387)
+
+# prompt for user input on coordinates of Point 1 and Point 2
+p1_1 = eval(input('Please enter Floor 1\'s Point 1 Coordinate in (x, y) form: '))
+p1_2 = eval(input('Please enter Floor 1\'s Point 2 Coordinate in (x, y) form: '))
+
+# p1_1, p1_2 = (2841, 2284), (2783, 2387)
 newp1_2 = ((p1_2[0]-p1_1[0]), (p1_2[1] - p1_1[1]))
 rot1 = -atan(newp1_2[1]/newp1_2[0])
 
@@ -34,13 +39,15 @@ xtr = xt*cos(rot1) - yt*sin(rot1)
 ytr = xt*sin(rot1) + yt*cos(rot1)
 ztr = z
 
-ax.plot_surface(xtr, ytr, z, facecolors=map1, shade=False, rstride=5, cstride=5)
+ax.plot_surface(xtr, ytr, z, facecolors=map1, shade=False, rstride=20, cstride=20)
 
 # translation and rotation of 2F
 
 x, y = np.mgrid[0:map2.shape[0], 0:map2.shape[1]]
 z = np.atleast_2d(10)
-p2_1, p2_2 = (2063, 1211), (2123, 1264)
+p2_1 = eval(input('Please enter Floor 2\'s Point 1 Coordinate in (x, y) form: '))
+p2_2 = eval(input('Please enter Floor 2\'s Point 2 Coordinate in (x, y) form: '))
+# p2_1, p2_2 = (2063, 1211), (2123, 1264)
 newp2_2 = ((p2_2[0]-p2_1[0]), (p2_2[1] - p2_1[1]))
 rot2 = -atan(newp2_2[1]/newp2_2[0])
 
@@ -52,13 +59,15 @@ xtr = xt*cos(rot2) - yt*sin(rot2)
 ytr = xt*sin(rot2) + yt*cos(rot2)
 ztr = z
 
-ax.plot_surface(xtr, ytr, z, facecolors=map2, shade=False, rstride=5, cstride=5)
+ax.plot_surface(xtr, ytr, z, facecolors=map2, shade=False, rstride=20, cstride=20)
 
 # translation and rotation of 3F
 
 x, y = np.mgrid[0:map3.shape[0], 0:map3.shape[1]]
 z = np.atleast_2d(20)
-p3_1, p3_2 = (693, 1462), (727, 1388)
+p3_1 = eval(input('Please enter Floor 3\'s Point 1 Coordinate in (x, y) form: '))
+p3_2 = eval(input('Please enter Floor 3\'s Point 2 Coordinate in (x, y) form: '))
+# p3_1, p3_2 = (693, 1462), (727, 1388)
 newp3_2 = ((p3_2[0]-p3_1[0]), (p3_2[1] - p3_1[1]))
 rot3 = -atan(newp3_2[1]/newp3_2[0])
 
@@ -71,7 +80,7 @@ xtr = xt*cos(rot3) - yt*sin(rot3)
 ytr = xt*sin(rot3) + yt*cos(rot3)
 ztr = z
 
-ax.plot_surface(xtr, ytr, z, facecolors=map3, shade=False, rstride=5, cstride=5)
+ax.plot_surface(xtr, ytr, z, facecolors=map3, shade=False, rstride=20, cstride=20)
 
 
 # ax.set_xlim(-1000, 1000)
